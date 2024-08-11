0781c75836b8808e0bc42425829e1663923de884
Chiem <c.p.stevens@student.tue.nl>
2024-08-11 20:35:47 +0200

    Don't need data objects

--- data_objects.py
+++
@@ -1,8 +0,0 @@
-class DataObjects:
-    def __init__(self):
-        self.data = []
-
-    def get_athlete_json(self, discordID, athleteID):
-        self.data.append({"discordID": discordID})
-        self.data.append({"athleteID": athleteID})
-        return self.data
